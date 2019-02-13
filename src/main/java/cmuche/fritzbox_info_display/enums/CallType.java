79d16336ee90e788644d6ae3efa4da690859a16f
Christoph Muche <christoph@muche.info>
2019-02-14 00:42:14 +0100

    + call type

---
+++ src/main/java/cmuche/fritzbox_info_display/enums/CallType.java
@@ -0,0 +1,6 @@
+package cmuche.fritzbox_info_display.enums;
+
+public enum CallType
+{
+  Inbound, Outbound, Missed
+}
--- src/main/java/cmuche/fritzbox_info_display/model/Call.java
+++ src/main/java/cmuche/fritzbox_info_display/model/Call.java
@@ -1,11 +1,14 @@
 package cmuche.fritzbox_info_display.model;
 
+import cmuche.fritzbox_info_display.enums.CallType;
+
 import java.util.Date;
 
-public class CallListEntry
+public class Call
 {
-  private String type;
+  private CallType type;
   private String numberInternal;
   private String numberExternal;
+  private String device;
   private Date date;
 }
