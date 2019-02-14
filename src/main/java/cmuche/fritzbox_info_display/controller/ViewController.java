41e8f7111378c144946d63dfb566f29aef6f97a0
Christoph Muche <christoph@muche.info>
2019-02-14 11:51:08 +0100

    display caller list in ui

--- src/main/java/cmuche/fritzbox_info_display/controller/ViewController.java
+++ src/main/java/cmuche/fritzbox_info_display/controller/ViewController.java
@@ -1,9 +1,11 @@
 package cmuche.fritzbox_info_display.controller;
 
+import cmuche.fritzbox_info_display.model.Call;
 import cmuche.fritzbox_info_display.model.DataResponse;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.util.Date;
 
@@ -15,9 +17,19 @@ public class ViewController
   @FXML
   private Label lblDate;
 
+  @FXML
+  private ListView lstCalls;
+
   public void displayData(DataResponse dataResponse)
   {
-
+    Platform.runLater(() ->
+    {
+      lstCalls.getItems().clear();
+      for (Call call : dataResponse.calls)
+      {
+        lstCalls.getItems().add(call);
+      }
+    });
   }
 
   public void displayTime(Date date)
