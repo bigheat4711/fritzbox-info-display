7e10e13fbec2b0b687abf3a658d15f271981004a
Christoph Muche <christoph@muche.info>
2019-02-14 11:50:19 +0100

    codes for mobile

--- src/main/java/cmuche/fritzbox_info_display/tools/CityCodeTool.java
+++ src/main/java/cmuche/fritzbox_info_display/tools/CityCodeTool.java
@@ -51,6 +51,36 @@ public class CityCodeTool
     {
       e.printStackTrace();
     }
+
+    codes.add(new CityCode("01570", "E-Plus"));
+    codes.add(new CityCode("01573", "E-Plus"));
+    codes.add(new CityCode("01575", "E-Plus"));
+    codes.add(new CityCode("01577", "E-Plus"));
+    codes.add(new CityCode("01578", "E-Plus"));
+    codes.add(new CityCode("0163", "E-Plus"));
+    codes.add(new CityCode("0177", "E-Plus"));
+    codes.add(new CityCode("0178", "E-Plus"));
+    codes.add(new CityCode("01590", "O2"));
+    codes.add(new CityCode("0176", "O2"));
+    codes.add(new CityCode("0179", "O2"));
+    codes.add(new CityCode("01520", "Vodafone"));
+    codes.add(new CityCode("01522", "Vodafone"));
+    codes.add(new CityCode("01523", "Vodafone"));
+    codes.add(new CityCode("01525", "Vodafone"));
+    codes.add(new CityCode("0162", "Vodafone"));
+    codes.add(new CityCode("0172", "Vodafone"));
+    codes.add(new CityCode("0173", "Vodafone"));
+    codes.add(new CityCode("0174", "Vodafone"));
+    codes.add(new CityCode("01511", "Deutsche Telekom"));
+    codes.add(new CityCode("01512", "Deutsche Telekom"));
+    codes.add(new CityCode("01514", "Deutsche Telekom"));
+    codes.add(new CityCode("01515", "Deutsche Telekom"));
+    codes.add(new CityCode("01516", "Deutsche Telekom"));
+    codes.add(new CityCode("01517", "Deutsche Telekom"));
+    codes.add(new CityCode("0160", "Deutsche Telekom"));
+    codes.add(new CityCode("0170", "Deutsche Telekom"));
+    codes.add(new CityCode("0171", "Deutsche Telekom"));
+    codes.add(new CityCode("0175", "Deutsche Telekom"));
   }
 
   public static CityCode getCityCodeForNumber(String number)
